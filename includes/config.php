44418fbb188aec9a88288101d6a5e160238041e1
EricDevr <acosta.eric40@gmail.com>
2023-02-01 19:51:17 -0300

    control de errores

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,5 @@
 <?php
 session_start();
 date_default_timezone_set("America/Santiago");
-<<<<<<< HEAD
 $URLBASE = "http://stlab.online";
-=======
-$URLBASE = "http://localhost:8003";
->>>>>>> 7a2bf56bc4a09c491bdb3380897143ba50a5867e
 ?>
--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 const URLBASE = "http://stlab.online";
-=======
-const URLBASE = "http://localhost:8003";
->>>>>>> 7a2bf56bc4a09c491bdb3380897143ba50a5867e
